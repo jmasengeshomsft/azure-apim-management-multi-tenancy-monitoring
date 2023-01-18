a37903b111081a3b3924953a67c11ce77a8a5b98
Joseph Masengesho <jmasengesho@inyarwanda.com>
2023-01-17 21:41:56 -0600

    added a folder for the main project

--- infrastructure/aad-entities/main.tf
+++ infrastructure/aad-entities/main.tf
@@ -1,21 +1,9 @@
-# module "tenant-a-aad-group" {
-#   source         = "../modules/aad-group/"
-#   tenant_name    = "${var.tenant_a_name}_xyz"
-#   app_owners_ids = [data.azuread_client_config.current.object_id]
-# }
-
 
 module "tenant-a-grafana-service-principal" {
   source         = "../modules/aad-service-principal/"
   tenant_name    = var.tenant_a_name
   app_owners_ids = [data.azuread_client_config.current.object_id]
 }
-
-# module "tenant-b-aad-group" {
-#   source         = "../modules/aad-group/"
-#   tenant_name    = var.tenant_b_name
-#   app_owners_ids = [data.azuread_client_config.current.object_id]
-# }
 
 module "tenant-b-grafana-service-principal" {
   source         = "../modules/aad-service-principal/"
